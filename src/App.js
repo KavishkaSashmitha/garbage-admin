4cb3a3dd4d61a0ce5336faf87d4a2b00130e257f
sithmi himanshi <sithmihimanshi@icloud.com>
2024-09-06 14:01:58 +0530

    create form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import TemporaryDrawer from './components/drawer';
-import AccountMenu from './components/navbar';
-import adminroutes from './pages/admin-routes';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TemporaryDrawer from "./components/drawer";
+import AccountMenu from "./components/navbar";
+import adminroutes from "./pages/admin-routes";
 
 function App() {
   return (
     <BrowserRouter>
       <AccountMenu />
       <Routes>
-        <Route path="/inbox" Component={adminroutes} />{' '}
+        <Route path="/inbox" Component={adminroutes} />{" "}
         {/* 👈 Renders at /app/ */}
       </Routes>
     </BrowserRouter>
